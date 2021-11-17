131db8c6037e1b4675e2f712058c9ae2034b70b1
Gitko97 <xcvdsf8216@gmail.com>
2021-11-17 20:58:42 +0900

    Door 클래스 연결, command line argument 추가

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import argparse
 import threading  # Thread
 import tkinter
 
-from controller import Controller
 from gui import App
 from model import Pose_Model
 from door import Door
@@ -11,18 +11,42 @@ os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 RESNET50_POOLING_AVERAGE = 'avg'
 DENSE_LAYER_ACTIVATION = 'softmax'
-OBJECTIVE_FUNCTION = 'categorical_crossentropy'
-LOSS_METRICS = ['accuracy']
-os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 weight_path = './best.hdf5'
 
 result_img = 0
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 if __name__ == "__main__":
-    pose_model = Pose_Model(RESNET50_POOLING_AVERAGE, DENSE_LAYER_ACTIVATION, weight_path=weight_path)
-    t1 = threading.Thread(target=pose_model.showProcess, args=())
-    t1.daemon = True
-    t1.start()
-    app = App(tkinter.Tk(), "GUI")
-    app.daemon = True
-    app.start()
-    # Door()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--door', '-d', help='Setting new door', default=False, dest='door', type=str2bool)
+    parser.add_argument('--phone', '-p', help='Setting phone number', default=False, dest='phone', type=str2bool)
+    parser.add_argument('--weight', '-w', help='Setting weight path', default="./best.hdf5", dest='weight_path')
+    parser.add_argument('--video', '-v', help='Input video path', dest='video')
+
+    set_door = parser.parse_args().door
+    set_phone = parser.parse_args().phone
+    weight_path = parser.parse_args().weight_path
+    video_path = parser.parse_args().video
+
+    video_path = './testVideo/test.mov' # debug path
+
+    door = Door()
+    set_door = door.read_position_file(set_door, video_path)
+
+    if not set_door and not set_phone:
+        pose_model = Pose_Model(RESNET50_POOLING_AVERAGE, DENSE_LAYER_ACTIVATION,video_path, weight_path=weight_path)
+        t1 = threading.Thread(target=pose_model.showProcess, args=())
+        t1.daemon = True
+        t1.start()
+        app = App(tkinter.Tk(), "GUI")
+        app.daemon = True
+        app.start()
--- model.py
+++ model.py
@@ -5,17 +5,16 @@ from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
 import gui
-from Managing_dementia_patients.door import Door
-
+import door
 
 class Pose_Model:
-    def __init__(self, RESNET50_POOLING_AVERAGE, DENSE_LAYER_ACTIVATION, weight_path = '/Users/joonhyoungjeon/PycharmProjects/pose-estimation/best.hdf5'):
+    def __init__(self, RESNET50_POOLING_AVERAGE, DENSE_LAYER_ACTIVATION, video_path, weight_path = '/Users/joonhyoungjeon/PycharmProjects/pose-estimation/best.hdf5'):
         self.model = Sequential()
         self.model.add(ResNet50(include_top=False, pooling=RESNET50_POOLING_AVERAGE, weights='imagenet'))
         self.model.add(Dense(2, activation=DENSE_LAYER_ACTIVATION))
         self.model.summary()
         self.model.load_weights(weight_path)
-        # self.door = Door()
+        self.video_path = video_path
 
     def detect_mp_pose(self, image, pose):
         new_image = image.copy()
@@ -25,6 +24,17 @@ class Pose_Model:
 
         landmarks = results.pose_landmarks.landmark
         return landmarks
+
+    def check_in_door(self, point):
+        door_min_x = door.min_point[0]
+        door_min_y = door.min_point[1]
+        door_max_x = door.max_point[0]
+        door_max_y = door.max_point[1]
+
+        if door_min_x < point[0] and point[0] < door_max_x:
+            if door_min_y < point[1] and point[1] < door_max_y:
+                return True
+        return False
 
     def draw_keyPoints_by_landmarks(self, image, landmarks, mp_pose):
         result_image = image.copy()
@@ -54,60 +64,76 @@ class Pose_Model:
                    (255, 0, 0), -1)
         return result_image
 
-    def showProcess(self, video_path='./testVideo/IMG_5348.MOV'):
-        cap = cv2.VideoCapture(video_path)
+    def showProcess(self):
+        cap = cv2.VideoCapture(self.video_path)
         fps = cap.get(cv2.CAP_PROP_FPS)
         print('FPS:', fps)
         mp_pose = mp.solutions.pose
         sitting_frame=0
         fps_discount = 0
+        previous_nose = 0
+        disappear_frame = 0
         with mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.5) as pose:
             while cap.isOpened():
+                if gui.stop:
+                    continue
                 ret, frame = cap.read()
 
                 if frame is None:
                     break
 
-                if fps_discount < 4:
-                    fps_discount = fps_discount + 1
-                    continue
-                fps_discount = 0
+                if gui.user_sleep is True:
+                    if fps_discount < 3:
+                        fps_discount = fps_discount + 1
+                        continue
+                    fps_discount = 0
 
                 image = frame
                 try:
-                    # cv2.rectangle(image, (self.door.min_point[0], self.door.min_point[1]),
-                    #               (self.door.max_point[0], self.door.max_point[1])
-                    #               , (0, 255, 0), 2)
-                    # if gui.user_sleep is False:
+                    cv2.rectangle(image, (door.min_point[0], door.min_point[1]),
+                                  (door.max_point[0], door.max_point[1])
+                                  , (0, 255, 0), 2)
+
                     landmarks = self.detect_mp_pose(image, pose)
 
-                    image = self.draw_keyPoints_by_landmarks(image,landmarks, mp_pose)
-                    predict_image = np.zeros_like(image)
-                    predict_image = self.draw_keyPoints_by_landmarks(predict_image,landmarks, mp_pose)
-
-                    predict_image = cv2.resize(predict_image, (224, 224))
-                    predict_image = np.array(predict_image).reshape((1,) + predict_image.shape) * (1. / 255)
-                    pred = self.model.predict(predict_image)
-                    predicted_class_indices = np.argmax(pred, axis=1)[0]
-                    if predicted_class_indices == 0:
-                        sitting_frame = 0
-                        predict = "Lying"
-                    else:
-                        sitting_frame = sitting_frame + 1
-                        predict = "Sitting"
-                        if sitting_frame == 10:
-                            sitting_frame = 0
-                            gui.user_sleep = False
                     image_shape = np.flip(image.shape[:2])
-                    L_shoulder = (landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-                                  landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y)
-                    cv2.putText(image, predict,
-                                tuple(np.multiply(L_shoulder, image_shape).astype(int)),
-                                cv2.FONT_HERSHEY_SIMPLEX, 10, (255, 255, 255), 2, cv2.LINE_AA
-                                )
+                    nose = (landmarks[mp_pose.PoseLandmark.NOSE.value].x, landmarks[mp_pose.PoseLandmark.NOSE.value].y)
+                    previous_nose = tuple(np.multiply(nose, image_shape).astype(int))
+
+                    disappear_frame = 0
+                    image = self.draw_keyPoints_by_landmarks(image,landmarks, mp_pose)
+                    if gui.user_sleep is True:
+                        predict_image = np.zeros_like(image)
+                        predict_image = self.draw_keyPoints_by_landmarks(predict_image,landmarks, mp_pose)
+
+                        predict_image = cv2.resize(predict_image, (224, 224))
+                        predict_image = np.array(predict_image).reshape((1,) + predict_image.shape) * (1. / 255)
+                        pred = self.model.predict(predict_image)
+                        predicted_class_indices = np.argmax(pred, axis=1)[0]
+                        if predicted_class_indices == 0:
+                            sitting_frame = 0
+                            predict = "Lying"
+                        else:
+                            sitting_frame = sitting_frame + 1
+                            predict = "Sitting"
+                            if sitting_frame == 10:
+                                sitting_frame = 0
+                                gui.user_sleep = False
+
+                        L_shoulder = (landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                                      landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y)
+                        cv2.putText(image, predict,
+                                    tuple(np.multiply(L_shoulder, image_shape).astype(int)),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 10, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
 
                 except Exception as e:
-                    print(e)
+                    disappear_frame = disappear_frame + 1
+                    if disappear_frame > 90 and previous_nose != 0:
+                        print("disappear")
+                        if self.check_in_door(previous_nose):
+                            print("In Door")
+                        previous_nose = 0
                     pass
                 gui.result_img = image
         cap.release()
